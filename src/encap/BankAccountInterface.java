16d268e275aa92f0eb62d5711b7fd338239b328c
biplab-w <waglebip@gmail.com>
2023-12-13 23:43:28 -0600

    Added interface for bank account

--- src/encap/BankAccountInterface.java
+++ src/encap/BankAccountInterface.java
@@ -1,11 +1,11 @@
 package encap;
 
 public interface BankAccountInterface {
-    public double deposit();
+    public double deposit(double amount);
     public double getBalance();
-    public double withdrawal();
-    public boolean transferAmount();
-    public double calculateInterest();
+    public double withdrawal(double amount);
+    public boolean transferAmount(BankAccount targetAccount, double amount);
+    public double calculateInterest(double annualRate);
 
 
 }
